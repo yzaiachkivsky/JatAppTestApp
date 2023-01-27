4d4ac6bae52e765710494ca867c09682b4b2d052
Yurii Zaiachkivskyi <yurii.zaiachkivskyi@avenga.com>
2023-01-27 15:37:54 +0200

    Initial Commit

---
+++ JatAppTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JatAppTestApp
+//
+//  Created by Yurii Zaiachkivskyi on 27.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
